fab98194ca5c2dce468844f7bb7e75c8b4c66a40
anhnguyen6529 <anhnguyen6529@gmail.com>
2020-06-22 19:42:08 +0700

    Update edited file except about, home, home_logined & preference

---
+++ js/item_info.js
@@ -0,0 +1,40 @@
+var submit = document.getElementsByClassName("submit");
+var orderButton = document.getElementsByClassName("order-button");
+var numOfItems = 0;
+
+for (var i = 0; i < orderButton.length; i++) {
+    orderButton[i].addEventListener("click",addToCart);
+}
+for (var i = 0; i < submit.length; i++) {
+    submit[i].addEventListener("click",addToCart);
+}
+
+function addToCart(){
+    alert("Accepted!")
+    numOfItems = numOfItems + 1;
+    document.querySelector(".cart_icon .badge").textContent = numOfItems;
+}
+
+function showAccOptions()
+{
+    var x = document.getElementById("op-active");
+    document.getElementById("acc").style.textDecoration = "none";
+    if (x.style.display === "block")
+    {
+        x.style.display = "none";
+    } else
+    {
+        x.style.display = "block";
+        x.style.position = "absolute";
+        x.style.background = "white";
+        x.style.width = "220px";
+        x.style.padding = "0px";
+        x.style.margin = "0";
+        x.style.left = "-145px";
+        x.style.top = "20px";
+        x.style.lineHeight = "30px";
+        x.style.zIndex = "1";
+        x.style.fontSize = "14px";
+    }
+    
+}
---
+++ js/pay_by_account.js
@@ -0,0 +1,23 @@
+function showAccOptions()
+{
+    var x = document.getElementById("op-active");
+    document.getElementById("acc").style.textDecoration = "none";
+    if (x.style.display === "block")
+    {
+        x.style.display = "none";
+    } else
+    {
+        x.style.display = "block";
+        x.style.position = "absolute";
+        x.style.background = "white";
+        x.style.width = "220px";
+        x.style.padding = "0px";
+        x.style.margin = "0";
+        x.style.left = "-135px";
+        x.style.top = "20px";
+        x.style.lineHeight = "30px";
+        x.style.zIndex = "1";
+        x.style.fontSize = "14px";
+    }
+    
+}
