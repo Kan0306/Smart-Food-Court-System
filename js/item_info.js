2348a77dd79c40749e5c09955f5dd37ce63b1bfe
anhnguyen6529 <anhnguyen6529@gmail.com>
2020-06-24 00:45:04 +0700

    Update popup, complete editing shopping_cart & link

--- js/item_info.js
+++ js/item_info.js
@@ -1,40 +1,27 @@
-var submit = document.getElementsByClassName("submit");
+var submit = document.getElementById("add_cart");
 var orderButton = document.getElementsByClassName("order-button");
 var numOfItems = 0;
 
 for (var i = 0; i < orderButton.length; i++) {
     orderButton[i].addEventListener("click",addToCart);
 }
-for (var i = 0; i < submit.length; i++) {
-    submit[i].addEventListener("click",addToCart);
-}
+submit.addEventListener("click",addToCartWithNum);
+
 
 function addToCart(){
     alert("Accepted!")
     numOfItems = numOfItems + 1;
     document.querySelector(".cart_icon .badge").textContent = numOfItems;
 }
-
-function showAccOptions()
-{
-    var x = document.getElementById("op-active");
-    document.getElementById("acc").style.textDecoration = "none";
-    if (x.style.display === "block")
+function addToCartWithNum()
+{  
+    var y = document.getElementById("num_item").value;
+    if (y === "0")
+        alert("Empty to add to cart!")
+    else
     {
-        x.style.display = "none";
-    } else
-    {
-        x.style.display = "block";
-        x.style.position = "absolute";
-        x.style.background = "white";
-        x.style.width = "220px";
-        x.style.padding = "0px";
-        x.style.margin = "0";
-        x.style.left = "-130px";
-        x.style.top = "20px";
-        x.style.lineHeight = "30px";
-        x.style.zIndex = "1";
-        x.style.fontSize = "14px";
+        alert("Accepted!")
+        numOfItems = numOfItems + Number(y);
+        document.querySelector(".cart_icon .badge").textContent = numOfItems;
     }
-    
 }
--- js/order.js
+++ js/order.js
@@ -10,27 +10,3 @@ function addToCart(){
     numOfItems = numOfItems + 1;
     document.querySelector(".cart_icon .badge").textContent = numOfItems;
 }
-
-function showAccOptions()
-{
-    var x = document.getElementById("op-active");
-    document.getElementById("acc").style.textDecoration = "none";
-    if (x.style.display === "block")
-    {
-        x.style.display = "none";
-    } else
-    {
-        x.style.display = "block";
-        x.style.position = "absolute";
-        x.style.background = "white";
-        x.style.width = "220px";
-        x.style.padding = "0px";
-        x.style.margin = "0";
-        x.style.left = "-150px";
-        x.style.top = "20px";
-        x.style.lineHeight = "30px";
-        x.style.zIndex = "1";
-        x.style.fontSize = "14px";
-    }
-    
-}
