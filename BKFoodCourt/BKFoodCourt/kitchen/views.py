f148e6023637a731033390e5283e772428e136a2
Kan0306 <vuhaiphu20000306@gmail.com>
2020-07-03 08:23:08 +0700

    Update process order of cook

---
+++ BKFoodCourt/BKFoodCourt/kitchen/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, Group
+from webapp.models import Order
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from webapp.decorators import allowed_users
+# Create your views here.
+
+@login_required
+#@allowed_users(['Cook'])
+def order_list(request):
+    orders = Order.objects.all();
+    
+    return render(request, 'kitchen/orderlist.html', {})
